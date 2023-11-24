f6e9cc39ecb3575548c330ba2460263bde607c16
Rahul <124366343+RaHuL79051@users.noreply.github.com>
2023-11-24 20:21:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMenu(){
+    const rules = document.querySelector('.rules');
+    rules.style.display = 'block';
+}
+
+function hideMenu(){
+    const rules = document.querySelector('.rules');
+    rules.style.display = 'none';
+}
+
+function GoToGame(){
+    window.location.href = 'game.html';
+}
